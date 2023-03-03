1cd2061e3b3721b5402afc5adbbd600d72a38658
aminebarka1212@gmail.com <aminebarka1212@gmail.com>
2023-03-03 14:31:55 +0100

    add:Hello from my BarkaBook!

--- src/extension/extension.ts
+++ src/extension/extension.ts
@@ -1,12 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+import { ExtensionContext, commands, window } from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  // Nothing (yet)
+export function activate(context: ExtensionContext) {
+
+  // Define a command for the button
+  const commandId = 'myExtension.sayHello';
+  context.subscriptions.push(
+    commands.registerCommand(commandId, () => {
+      window.showInformationMessage('Hello from my BarkaBook!');
+      addCodeToActiveCell();
+    })
+  );
+
+  // Create a button on the toolbar of the notebook editor
+  const button = window.createStatusBarItem(undefined, 100); 
+  button.text = 'Say Hello';
+  button.command = commandId;
+  button.show();
+  context.subscriptions.push(button);
+
+  // Add code to the active cell
+  function addCodeToActiveCell(): void {
+    const editor = window.activeNotebookEditor;
+    if (!editor) {
+      window.showErrorMessage('No notebook editor available!');
+      return;
+    }
+    const cell = editor.notebook.cellAt;
+    if (!cell) {
+      window.showErrorMessage('No active cell available!');
+      return;
+    }
+    const position = cell.apply;
+    position.caller.('console.log("Hello friend !");\n');
+  }
 }
 
-// This method is called when your extension is deactivated
-export function deactivate() { }
+export function deactivate() {}
